728420c7fab6c820a2f022e2a0f193f39c1d1dd3
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-07-24 17:27:43 -0600

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -37,42 +37,25 @@ module.exports.getAuthURL = async () => {
   };
 };
 
-module.exports.getAccessToken = (event) => {
+module.exports.getAccessToken = async (event) => {
   // Decode authorization code extracted from the URL query
-  const code = decodeURIComponent(`${event.queryStringParameters.code}`);  
+  const code = decodeURIComponent(`${event.queryStringParameters.code}`); 
+  const res = await oAuth2Client.getToken(code);
+  if (!res.tokens) {
+    console.log('Invalid authorization code')
+    return {
+      statusCode: 500,
+      body: JSON.stringify({error: 'Invalid authorization code'}),
+    };
+  }
 
-  return new Promise((resolve, reject) => {
-    /**
-     *  Exchange authorization code for access token with a “callback” after the exchange,
-     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
-     */
-
-    oAuth2Client.getToken(code, (error, response) => {
-      if (error) {
-        // console.log("oauth error: " , error);
-        return reject(error);
-      }
-      return resolve(response);
-    });
-  })
-    .then((results) => {
-      // Respond with OAuth token 
-      console.log(results);
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-        },
-        body: JSON.stringify(results),
-      };
-    })
-    .catch((error) => {
-      // Handle error
-      // console.log("error: " , error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify(error),
-      };
-    });
+  console.log(res.tokens)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(res.tokens),
+  };
 };
