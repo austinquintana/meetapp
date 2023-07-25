d713537bd33dae46db3e464e63bd3ec81c350e93
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-07-24 21:00:22 -0600

    Added Calendar Events

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,23 +1,24 @@
-'use strict';
+"use strict";
 
 const { google } = require("googleapis");
 // const { concatenateToResponse } = require("workbox-streams");
-const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
-const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = [
-  "https://austinquintana.github.io/meetapp/"
+const calendar = google.calendar("v3");
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar.events.public.readonly",
 ];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = ["https://austinquintana.github.io/meetapp/"];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
-  redirect_uris[0]
+  redirect_uris[0],
 );
 
 module.exports.getAuthURL = async () => {
   /**
    *
-   * Scopes array is passed to the `scope` option. 
+   * Scopes array is passed to the `scope` option.
    *
    */
   const authUrl = oAuth2Client.generateAuthUrl({
@@ -28,8 +29,8 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
     },
     body: JSON.stringify({
       authUrl,
@@ -39,23 +40,75 @@ module.exports.getAuthURL = async () => {
 
 module.exports.getAccessToken = async (event) => {
   // Decode authorization code extracted from the URL query
-  const code = decodeURIComponent(`${event.queryStringParameters.code}`); 
-  const res = await oAuth2Client.getToken(code);
-  if (!res.tokens) {
-    console.log('Invalid authorization code')
+  const code = decodeURIComponent(`${event.queryStringParameters.code}`);
+  try {
+    const res = await oAuth2Client.getToken(code);
+
+    console.log(res.tokens);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(res.tokens),
+    };
+  } catch (e) {
     return {
       statusCode: 500,
-      body: JSON.stringify({error: 'Invalid authorization code'}),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(e),
     };
-  }
-
-  console.log(res.tokens)
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(res.tokens),
   };
+};
+
+  module.exports.getCalenderEvents = async (event) => {
+    const oAuth2Client = new google.auth.OAuth2(
+      CLIENT_ID,
+      CLIENT_SECRET,
+      redirect_uris[0]
+    );
+    const access_token = decodeURIComponent(
+      `${event.pathParameters.access_token}`
+  );
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+  .then((results) => {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({ events: results.data.items })
+    };
+  })
+  .catch((err) => {
+    // Handle error
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err),
+    };
+  });
 };
