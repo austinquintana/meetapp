6f2d6f1603daa89d0c1dfa14eafd1beda55e2f7f
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-08-07 22:56:42 -0600

    Added new components and tests

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-/// src/App.js
-
+import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
 const App = () => {
- return (
-   <div className="App">
-     <div id="event-list"></div>
-   </div>
- );
-}
+  return (
+    <div className="App">
+      <NumberOfEvents></NumberOfEvents>
+      <EventList></EventList>
+      <CitySearch></CitySearch>
+    </div>
+  );
+};
 
 export default App;
