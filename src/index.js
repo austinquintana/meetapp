2475803ed37003353f99d6651aabf6cdcd5cc1c8
austinquintana <124468025+austinquintana@users.noreply.github.com>
2023-08-11 11:51:29 -0600

    Added atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('0e08ffb20eef412f8bd610435c7ce936').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
